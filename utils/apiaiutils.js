641dfca4e7fc94175e78b62a4b5780fbe70db24d
ctchidu <30950313+ctchidu@users.noreply.github.com>
2017-08-19 12:25:52 +0530

    Update apiaiutils.js

--- utils/apiaiutils.js
+++ utils/apiaiutils.js
@@ -33,32 +33,6 @@ function convertToJSON(array) {
   return jsonData;
 };
 
-function prepareSendBio() {
-  let messageData = {
-    message: {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'generic',
-          elements: [{
-            title: 'chidu',
-            subtitle: 'chidu1',
-            item_url: 'https://www.aircanada.com/content/dam/aircanada/portal/images/content-images/plan/baggage/std-article.png',
-            image_url: 'https://www.aircanada.com/content/dam/aircanada/portal/images/content-images/plan/baggage/std-article.png',
-            buttons: [{
-              type: 'web_url',
-              url: 'https://www.aircanada.com/content/dam/aircanada/portal/images/content-images/plan/baggage/std-article.png',
-              title: 'testing'
-            }],
-          }]
-        }
-      }
-    }
-  };
-	
-  return messageData;
-};
-
 
 
 
@@ -138,121 +112,15 @@ var fulfillmentRequest = function(request, response) {
               
              response.json(json);
              break;
-                
-                
-             case 'checkedcarryon':
-                
-                var checkAccepted = ["Archery equipment – bows and arrows","Archery equipment – bows and arrows","Archery equipment",
-                    "bows and arrows","bows","arrows","Avalanche rescue equipment","Avalanche rescue equipment",
-                    "Bicycles","Bicycles","cycle","cycles","Boating/Fishing equipment","Boating/Fishing equipment",
-                    "Boating","Fishing","Fishing equipment","Boating equipment","Boogie boards","Boogie boards","skim",
-                    "speed","skateboards","Skates","Skeleton Sleds","Skis","snowboards","Broomball","Broomball",
-                    "curling equipment","curling equipment","Golf Clubs","Golf Clubs","golf","golf stick",
-                    "Hockey","Hockey","lacrosse","ringette","cricket","cricket equipment",
-                    "Kayaking equipment","Kayaking equipment","Kayaking",
-                    "Parachutes","Parachutes","parachuting","parachuting equipment",
-                    "Racquets","Racquets","badminton","squash","tennis",
-                    "Scuba diving equipment","Scuba diving equipment","Scuba diving","scuba","diving",
-                    "bowling balls","bowling balls","Sports balls","balls","ball",
-                    "Surfboards","paddleboards","kiteboards","wakeboards","wave skis","Carry-on bag","Carry-on bag","carry on bag","roller bag","backpack","briefcase",
-                    "laptop","laptop",
-                    "Cat","Cat","small dog","pets",
-                    "duty-free purchases","duty-free purchases","duty free",
-                    "Sport racquet","Sport racquet","tennis","squash","badminton","musical instrument",
-                    "Camera bag","Camera bag","diplomatic","consular bag","Garment bag",
-                    "guitars","guitars","violins","violas","golf club"];
-                
-                
-                
-                 var checkRejected = ["Bobsleighs","Bobsleighs",
-                        "Canoes","Canoes",
-                        "Hang gliders","Hang gliders","gliders",
-                        "Luges","Luges",
-                        "Vaulting poles","Vaulting poles","vaulting",
-                        "Windsurfing equipment","Windsurfing equipment","Windsurfing"];
-                
-    var carryAccepted = ["Carry-on bag","Carry-on bag","carry on bag","roller bag","backpack","briefcase",
-                        "laptop","laptop",
-                        "Cat","Cat","small dog","pets",
-                        "duty-free purchases","duty-free purchases","duty free",
-                        "Sport racquet","Sport racquet","tennis","squash","badminton","musical instrument",
-                        "Camera bag","Camera bag","diplomatic","consular bag","Garment bag",
-                        "guitars","guitars","violins","violas"];
-
-            var restricted = ["Batteries","Batteries",
-                        "Battery-powered","Battery-powered","battery powered",
-                        "Camping Equipment","Camping Equipment",
-                        "Curling Irons","Curling Irons","Lighters",
-                        "Dry ice","Dry ice","Brine","Gel","ice Packs","Liquids",
-                        "aerosols","aerosols","toiletries","alcoholic beverages","alcohol",
-                        "Meals","Meals","Ready-to-eat","ready to eat",
-                        "Oxygen","Oxygen",
-                        "Air Purifiers","Air Purifiers","Ionizers",
-                        "Avalanche Rescue Backpacks","Avalanche Rescue Backpacks",
-                        "Compressed Gas","Compressed Gas","Cylinders",
-                        "Corrosive","Corrosive","Oxidizing Materials","oxidize",
-                        "Defence","Defence","Incapacitating","Explosives",
-                        "Firearms","Firearms","Ammunition",
-                        "Fuel-powered Equipment","Fuel-powered Equipment","Fuel powered",
-                        "Paint","Paint","Poisons","Toxins",
-                        "Radioactive Materials","Radioactive Materials",
-                        "Security Attaché Cases","Security Attaché Cases"];  
-              
-                var typeofbaggage = body.result.parameters.typeofbaggage;
-                var splItems = body.result.parameters.splItems;
-                
-             
-                   if(typeofbaggage == 'checked baggage')
-                {
-                    if(checkAccepted.indexOf(splItems) > -1){
-                     var json = formatApiaiResponse(speech='Yes, you can carry it is  accepted as checked baggage.\nFor more queries related to checked baggage, Kindly refer the below link.\nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/special-items.html', 
-                     displayText='Yes, you can carry it is  accepted as checked baggage.\nFor more queries related to checked baggage, Kindly refer the below link.\n https://www.aircanada.com/ca/en/aco/home/plan/baggage/special-items.html')
-                  
-                    }
-                    else if(checkRejected.indexOf(splItems) > -1){
-                     var json = formatApiaiResponse(speech='No,It is not accepted as checked baggage because of their overall dimensions.\nFor more queries related to checked baggage, Kindly refer the below link. \nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/special-items.html',
-                     displayText='No,It is not accepted as checked baggage because of their overall dimensions.\nFor more queries related to checked baggage, Kindly refer the below link. \nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/special-items.html')
-                         
-                        }else if(restricted.indexOf(splItems) > -1){
-                        var json = formatApiaiResponse(speech='No,It is Restricted / Prohibited in either carry-on or checked baggage..\nFor more queries related to Restricted / Prohibited items, Kindly refer the below link. \nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/restricted-and-prohibited-items.html',
-                     displayText='No,It is Restricted / Prohibited in either carry-on or checked baggage...\nFor more queries related to Restricted / Prohibited items, Kindly refer the below link. \nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/restricted-and-prohibited-items.html')
-                        
-                        }else{
-                            var json = formatApiaiResponse(speech='Sorry not able to answer you at the moment',displayText='Sorry not able to answer you at the moment')
-                        }
-    
-                
-                }   
-                else if(typeofbaggage == 'carry on baggage'){
-                    
-                    if(carryAccepted.indexOf(splItems) > -1){
-                       var json = formatApiaiResponse(speech='Yes, you can carry it is  accepted as carry on baggage.\nFor more queries related to carry on baggage, Kindly refer the below link.\nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/carry-on.html', 
-                     displayText='Yes, you can carry it is  accepted as carry on baggage.\nFor more queries related to carry on baggage, Kindly refer the below link.\nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/carry-on.html')
-                  
-                    }
-                    else if(restricted.indexOf(splItems) > -1){
-                        var json = formatApiaiResponse(speech='No,It is Restricted / Prohibited in either carry-on or checked baggage..\nFor more queries related to Restricted / Prohibited items, Kindly refer the below link. \nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/restricted-and-prohibited-items.html',
-                     displayText='No,It is Restricted / Prohibited in either carry-on or checked baggage...\nFor more queries related to Restricted / Prohibited items, Kindly refer the below link. \nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/restricted-and-prohibited-items.html')
-                        
-                        }
-                    else{
-                           var json = formatApiaiResponse(speech='No,It is not accepted as carry on baggage because of their overall dimensions.\nFor more queries related to carry on baggage, Kindly refer the below link. \nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/carry-on.html',
-                     displayText='No,It is not accepted as carry on baggage because of their overall dimensions.\nFor more queries related to carry on baggage, Kindly refer the below link. \nhttps://www.aircanada.com/ca/en/aco/home/plan/baggage/carry-on.html')
-                    
-                        }
-                    
-                }
-                
-               
-             response.json(json);
-             break;   
+  
                 
          case 'all.items':
 
 		var allitems = body.result.parameters.allitems;
 		var typeofbaggage = body.result.parameters.typeofbaggage;	
 		if(allitems == 'sports'){
-                    var json = prepareSendBio();
+                        var str = fs.readFileSync('./sports.txt', 'utf8');
+                        var json = formatApiaiResponse(speech = str,displayText = str)
                     }
                 if(allitems == 'music'){
                         var str = fs.readFileSync('./music.txt', 'utf8');
