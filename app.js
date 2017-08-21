f4e7991f34513a9d3d6c98d7af506e30eb01cc74
ctchidu <30950313+ctchidu@users.noreply.github.com>
2017-08-21 11:51:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ app.post('/facebook', json_body_parser, function(req, response) {
         data.entry.forEach(pageEntry => {
             pageEntry.messaging.forEach(messagingEvent => {
                 if (messagingEvent.message) {
+                    console.log('entry here');
+                    console.log(messagingEvent.message);
                     if (!messagingEvent.message.is_echo) {
                         facebookUtils.handleFacebookTextMessage(messagingEvent);
                     }
