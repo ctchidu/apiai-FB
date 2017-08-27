d1d56c186a7d9d20cb0da23660865fc815442dee
ctchidu <30950313+ctchidu@users.noreply.github.com>
2017-08-27 13:09:23 +0530

    Add files via upload

---
+++ utils/greeting.js
@@ -0,0 +1,52 @@
+var request = require('request');
+var config = require('../config.js').getConfig();
+
+var apiai = require('apiai')
+var api = apiai(config.apiaitoken);
+
+
+var facebookGreeting = function(event) {
+	var sender_id = event.sender.id;
+		let messageData = {
+			  "recipient":{
+				"id":sender_id
+			  },
+			"message":{
+				"text": "Hello!! Warm Greetings :) How can I help you with Baggage related queries ?",
+				}
+		}	
+                sendMessage(messageData);
+}
+
+
+// Send a message via the Facebook Graph API
+var sendMessage = function(messageData) {
+    request({
+        uri: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: config.facebook.pageAccessToken },
+        method: "POST",
+        json: messageData
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            if (messageId) {
+                console.log("Successfully sent message with id %s to recipient %s",
+                messageId, recipientId);
+            } else {
+                console.log("Successfully called Send API for recipient %s",
+                recipientId);
+            }
+        } else {
+            console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+        }
+    });
+}
+
+
+
+
+module.exports = {	
+    facebookGreeting
+}
