05369a9c5ec364cd0dfcc076ecbe8bdb80dcb537
ctchidu <30950313+ctchidu@users.noreply.github.com>
2017-09-05 14:52:27 +0530

    Update apiaiutils.js

--- utils/apiaiutils.js
+++ utils/apiaiutils.js
@@ -68,11 +68,11 @@ var fulfillmentRequest = function(request, response) {
 			parseXlsx('BAG_FARE_DATA.xlsx', function(err, data) {	
 			var jsonData = JSON.parse(JSON.stringify(convertToJSON(data)));
 			for(i = 0; i < jsonData.length; i++){
+		 if((jsonData[i].COUNTRY == locationcountry) && (jsonData[i].ALTITUDE == frequentfly) && (jsonData[i].CLASS == travelclass)){
 			console.log(jsonData[i].COUNTRY);
 			console.log(jsonData[i].ALTITUDE);
 			console.log(jsonData[i].CLASS);	
-		 if((jsonData[i].COUNTRY == locationcountry) && (jsonData[i].ALTITUDE == frequentfly) && (jsonData[i].CLASS == travelclass)){
-			console.log(jsonData[i].FARE);	
+			 console.log(jsonData[i].FARE);	
 			 var json = formatApiaiResponse(speech = jsonData[i].FARE,displayText = jsonData[i].FARE)
 
 				 }	 
