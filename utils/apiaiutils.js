b1d6c3a6ad124acc9c48e891ffa90635e6bd5f99
ctchidu <30950313+ctchidu@users.noreply.github.com>
2017-09-13 18:11:33 +0530

    Update apiaiutils.js

--- utils/apiaiutils.js
+++ utils/apiaiutils.js
@@ -125,128 +125,92 @@ var fulfillmentRequest = function(request, response) {
 		if(allitems == 'carry on baggage'){
                         var str = fs.readFileSync('./carryon.txt', 'utf8');
                         var json = {
-				  "speech":"This is a simple response for a carousel",
-				  "data": {
-				    "google":
-				    {
-				      "expectUserResponse":true,
-				      "richResponse":
-				      {
-					"items":
-					[
-					  {
-					    "simpleResponse":
+					  "speech":"This is a simple response for a carousel",
+					  "data": {
+					    "google":
 					    {
-					      "textToSpeech":"This is a simple response for a carousel"
-					    }
-					  }
-					],
-					"suggestions":
-					[
-					  {
-					    "title":"Basic Card"
-					  },
-					  {
-					    "title":"List"
-					  },
-					  {
-					    "title":"Carousel"
-					  },
-					  {
-					    "title":"Suggestions"
-					  }
-					]
-				      },
-				      "systemIntent":
-				      {
-					"intent":"actions.intent.OPTION",
-					"data":
-					{
-					  "@type":"type.googleapis.com/google.actions.v2.OptionValueSpec",
-					  "carouselSelect":
-					  {
-					    "items":
-					    [
+					      "expectUserResponse":true,
+					      "richResponse":
 					      {
-						"optionInfo":
-						{
-						  "key":"title",
-						  "synonyms":
-						  [
-						    "synonym of title 1",
-						    "synonym of title 2",
-						    "synonym of title 3"
-						  ]
-						},
-						"title":"Title of First List Item",
-						"description":"This is a description of a carousel item",
-						"image":
-						{
-						  "url":"https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-						  "accessibilityText":"Image alternate text"
-						}
+						"items":
+						[
+						  {
+						    "simpleResponse":
+						    {
+						      "textToSpeech":str
+						    }
+						  }
+						],
+						"suggestions":
+						[
+						  {
+						    "title":"Basic Card"
+						  },
+						  {
+						    "title":"List"
+						  },
+						  {
+						    "title":"Carousel"
+						  },
+						  {
+						    "title":"Suggestions"
+						  }
+						]
 					      },
+					      "systemIntent":
 					      {
-						"optionInfo":
+						"intent":"actions.intent.OPTION",
+						"data":
 						{
-						  "key":"googleHome",
-						  "synonyms":
-						  [
-						    "Google Home Assistant",
-						    "Assistant on the Google Home"
-						  ]
-						},
-						"title":"Google Home",
-						"description":"Google Home is a voice-activated speaker powered by the Google Assistant.",
-						"image":
-						{
-						  "url":"https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
-						  "accessibilityText":"Google Home"
-						}
-					      },
-					      {
-						"optionInfo":
-						{
-						  "key":"googlePixel",
-						  "synonyms":
-						  [
-						    "Google Pixel XL",
-						    "Pixel",
-						    "Pixel XL"
-						  ]
-						},
-						"title":"Google Pixel",
-						"description":"Pixel. Phone by Google.",
-						"image":
-						{
-						  "url":"https://storage.googleapis.com/madebygoog/v1/Pixel/Pixel_ColorPicker/Pixel_Device_Angled_Black-720w.png",
-						  "accessibilityText":"Google Pixel"
-						}
-					      },
-					      {
-						"optionInfo":
-						{
-						  "key":"googleAllo",
-						  "synonyms":
-						  [
-						    "Allo"
-						  ]
-						},
-						"title":"Google Allo",
-						"description":"Introducing Google Allo, a smart messaging app that helps you say more and do more.",
-						"image":
-						{
-						  "url":"https://allo.google.com/images/allo-logo.png",
-						  "accessibilityText":"Google Allo Logo"
+						  "@type":"type.googleapis.com/google.actions.v2.OptionValueSpec",
+						  "carouselSelect":
+						  {
+						    "items":
+						    [
+						      {
+							"optionInfo":
+							{
+							  "key":"title",
+							  "synonyms":
+							  [
+							    "synonym of title 1",
+							    "synonym of title 2",
+							    "synonym of title 3"
+							  ]
+							},
+							"title":"Standard Article",
+							"description":"This is a description of a carousel item",
+							"image":
+							{
+							  "url":"https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+							  "accessibilityText":"Image alternate text"
+							}
+						      },
+						      {
+							"optionInfo":
+							{
+							  "key":"googleHome",
+							  "synonyms":
+							  [
+							    "Google Home Assistant",
+							    "Assistant on the Google Home"
+							  ]
+							},
+							"title":"Personal Article",
+							"description":"Google Home is a voice-activated speaker powered by the Google Assistant.",
+							"image":
+							{
+							  "url":"https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
+							  "accessibilityText":"Google Home"
+							}
+						      }
+						    ]
+						  }
 						}
 					      }
-					    ]
+					    }
 					  }
-					}
-				      }
-				    }
-				  }
-				};
+					};
                     }	
                 response.json(json);
                 break;   
